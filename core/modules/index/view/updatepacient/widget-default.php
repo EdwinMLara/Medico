50675bb6edd6fa2dc7d8704a0cfdfb2e19c54725
EdwinMLara <emlara35@gmail.com>
2019-12-20 11:11:34 -0600

    Agregando redireccion al actulizar las alergias en el paciente

--- core/modules/index/view/updatepacient/widget-default.php
+++ core/modules/index/view/updatepacient/widget-default.php
@@ -1,13 +1,12 @@
 <?php
 
 if(count($_POST)>0){
-	echo $_POST["user_id"];
 	$user = PacientData::getById($_POST["user_id"]);
 	$user->name = $_POST["name"];
 	$user->lastname = $_POST["lastname"];
 	$user->alergias = $_POST["alergias"];
 	$user->update();
 	Core::alert("Actualizado exitosamente!");
-	print "<script>window.location='index.php?view=pacients';</script>";
+	print "<script>window.location='index.php?view=newreservation2&id=".$_POST["user_id"]."&id_beneficiario=$user->id_beneficiario&id_titular=$user->id_titular';</script>";
 }
 ?>
