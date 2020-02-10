633f10141a18993dac6d4a2811fe8d1c871f8a99
EdwinMLara <emlara35@gmail.com>
2020-02-09 23:06:29 -0600

    agregando archivos para redireccion al actualizar los datos y acceder al modulo de consulta

--- core/modules/index/view/addincumbent/widget-default.php
+++ core/modules/index/view/addincumbent/widget-default.php
@@ -9,17 +9,31 @@ if(count($_POST)>0){
 	$Apellidos = ucwords($_POST["Apellido"]);
 	$titular_aux;
 
+	$beneficiario_id_aux = 0;
+	$paciente_id_aux = 0;
+	$titular_id_aux = 0;
+	$bandera = 0;
+
 	if($titular_aux =  IncumbentData::getId_titular($Nombre,$Apellidos)){
 		$paciente = PacientData::getId_paciente($Nombre,$Apellidos);
 		IncumbentData::update_ruta_foto($titular->Ruta_foto,$titular_aux->id_titular);
 		PacientData::update_id_titular($titular_aux->id_titular,$paciente->id_paciente);
 		$mensaje = "Se ha actualizado la foto";
+		$bandera = 1;
+		$paciente_id_aux = $paciente->id_paciente;
+		$titular_id_aux = $titular_aux->id_titular;
+		$beneficiario_id_aux = $paciente->id_beneficiario;
+		
 	}else{
 		$titular->insert(1);
 		if($paciente = PacientData::getId_paciente($Nombre,$Apellidos)){
 			$titular_aux = IncumbentData::getId_titular($Nombre,$Apellidos); 
 			PacientData::update_id_titular($titular_aux->id_titular,$paciente->id_paciente);
 			$mensaje = "Se ha actualizado la foto";
+			$bandera = 1;
+			$paciente_id_aux = $paciente->id_paciente;
+			$titular_id_aux = $titular_aux->id_titular;
+			$beneficiario_id_aux = $paciente->id_beneficiario;
 		}else{
 			$paciente = new PacientData();
 			$paciente->name = $titular->Nombre;
@@ -35,8 +49,10 @@ if(count($_POST)>0){
 	}
 	
 	Core::alert($mensaje);
-	print "<script>window.location='index.php?view=incumbent';</script>";
-
-	
+	if($bandera){
+		print "<script>window.location='index.php?view=newreservation2&id=".$paciente_id_aux."&id_beneficiario=".$beneficiario_id_aux."&id_titular=".$titular_id_aux."';</script>";
+	}else{
+		print "<script>window.location='index.php?view=incumbent';</script>";
+	}	
 }
 ?>
