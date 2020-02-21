84fe37b33b579dfbb7c94ef6569fb7d9dad8ffc3
EdwinMLara <emlara35@gmail.com>
2020-02-21 14:26:34 -0600

    agregando botones para motrar pacientes activos e inactivos

--- core/modules/index/view/pacients/widget-default.php
+++ core/modules/index/view/pacients/widget-default.php
@@ -1,22 +1,44 @@
 <div class="row">
 	<div class="col-md-12">
 		<h1>Pacientes</h1>
-		<br>
 		<?php
 		$id_usuario = $_SESSION["user_id"];
 		$user_aux = UserData:: getById($id_usuario);
+		$str = "";
+		$badera_activacion = 0;
 		//echo $user_aux->id." ".$user_aux->is_admin;
-		if($user_aux->is_admin){
-			$users = PacientData::getAll_disable();
+		if($user_aux->is_admin){ ?> 
+			<a href="index.php?view=pacients&activos=1"> <button type="button" class="btn btn-primary">Activos</button></a>
+			<a href="index.php?view=pacients&activos=0"> <button type="button" class="btn btn btn-info">Inactivos</button></a>
+			<br>
+		<?php
+			if(isset($_GET["activos"])){
+				if((int)$_GET["activos"]){
+					$users = PacientData::getAll();
+					$badera_activacion = 1;
+					$str = "activos";
+				}else{
+					$users = PacientData::getAll_disable();
+					$str = "inactivos";
+					$badera_activacion = 0;
+				}	
+			}else{
+				$badera_activacion = 0;
+				$users = PacientData::getAll_disable();
+				$str = "inactivos";
+			}
 		}else{
 			$users = PacientData::getAll();
+			$str = "activos";
+			$badera_activacion = 1; 
 		}
 		if(count($users)>0){
 			// si hay usuarios
 			?>
+			<br>
 			<table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
 				<thead>
-					<th>Nombre completo</th>
+					<th>Nombre completo <?php echo $str; ?> </th>
 					<th></th>
 				</thead>
 				<?php
@@ -28,7 +50,7 @@
 						<a href="index.php?view=newreservation2&id=<?php echo $user->id_paciente;?>&id_beneficiario=<?php echo $user->id_beneficiario;?>&id_titular=<?php echo $user->id_titular?>" class="btn btn-success btn-xs">Nueva Cita</a>
 						<a href="index.php?view=pacienthistory&id=<?php echo $user->id_paciente;?>" class="btn btn-info btn-xs">Historial</a>
 						<?php
-						if($user_aux->is_admin){ ?>
+						if(!$badera_activacion){ ?>
 							<a href="index.php?view=pacientdesactivate&id=<?php echo $user->id_paciente;?>&active=1" class="btn btn-warning btn-xs">activar</a>
 						<?php
 						}else{ ?>
