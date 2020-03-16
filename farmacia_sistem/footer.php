927b1aa5ac770553ba5e71f461cb6fa53030b85c
EdwinMLara <emlara35@gmail.com>
2020-03-15 23:50:38 -0600

    eliminando la clave en el atributo integrity del head y footer en el sistema de farmacia, ya que inpedia el funcionamiento de los iconos de bootstrap en la pc Personal

--- farmacia_sistem/footer.php
+++ farmacia_sistem/footer.php
@@ -5,8 +5,8 @@
       </p>
    </div>
 </div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <!-- Latest compiled and minified JavaScript -->
-	<script src="js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<!-- Latest compiled and minified JavaScript -->
+<script src="js/bootstrap.min.js" integrity="" crossorigin="anonymous"></script>
