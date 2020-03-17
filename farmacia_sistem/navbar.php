c5f5797ed9cd28f75fb655d51e83816a1195d90a
EdwinMLara <emlara35@gmail.com>
2020-03-16 22:06:58 -0600

    se elimino del menu de navegacion la pestaña usuarios, ya que no es necesaria para las usuarios del tipo farmacia

--- farmacia_sistem/navbar.php
+++ farmacia_sistem/navbar.php
@@ -17,7 +17,7 @@
 				<li class="<?php echo $active_facturas;?>"><a href="facturas.php"><i class='glyphicon glyphicon-list-alt'></i> Recetas <span class="sr-only">(current)</span></a></li>
 				<li class="<?php echo $active_productos;?>"><a href="productos.php"><i class='glyphicon glyphicon-barcode'></i> Productos</a></li>
 				<li class="<?php echo $active_clientes;?>"><a href="clientes.php"><i class='glyphicon glyphicon-user'></i> Clientes</a></li>
-				<li class="<?php echo $active_usuarios;?>"><a href="usuarios.php"><i class='glyphicon glyphicon-lock'></i> Usuarios</a></li>
+				<!--<li class="<?php echo $active_usuarios;?>"><a href="usuarios.php"><i class='glyphicon glyphicon-lock'></i> Usuarios</a></li>-->
 				<li class="<?php ?>"><a href="departamentos.php"><i class='glyphicon glyphicon-lock'></i> Departamentos</a></li>
 				<li class="<?php if(isset($active_perfil)){echo $active_perfil;}?>"><a href="perfil.php"><i class='glyphicon glyphicon-cog'></i> Configuración</a></li>
 				<li class="dropdown">
